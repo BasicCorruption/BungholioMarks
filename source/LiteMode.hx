e157047953c501e7d5d556ecd14ba9e98126383c
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-30 11:15:08 -0400

    update rendering method

--- source/LiteMode.hx
+++ source/LiteMode.hx
@@ -69,6 +69,8 @@ class LiteMode extends FlxState
 			FlxG.fullscreen = !FlxG.fullscreen;
 		}
 
+		time += elapsed;
+
 		var p = pixels.data.getInt32(4 * (px + py * pixels.width));
 		var a:Int = p >>> 24;
 		var r:Int = (p >>> 16) & 0xff;
@@ -76,15 +78,72 @@ class LiteMode extends FlxState
 		var b:Int = (p) & 0xff;
 		var hex:String = StringTools.hex(p, 8);
 
-		time += elapsed;
+		if (r > 5 && g > 5 && b > 5)
+		{
+			var s = new FlxSprite();
+			s.makeGraphic(1, 1, FlxColor.fromRGB(r, g, b, a));
+			s.x = px;
+			s.y = py;
+			add(s);
+			renders++;
+		}
 
-		var s = new FlxSprite();
-		s.makeGraphic(1, 1, FlxColor.fromRGB(r, g, b, a));
-		s.x = px;
-		s.y = py;
-		add(s);
+		px++;
 
-		renders++;
+		var p2 = pixels.data.getInt32(4 * (px + py * pixels.width));
+		var a2:Int = p >>> 24;
+		var r2:Int = (p >>> 16) & 0xff;
+		var g2:Int = (p >>> 8) & 0xff;
+		var b2:Int = (p) & 0xff;
+		var hex2:String = StringTools.hex(p2, 8);
+
+		if (r2 > 5 && g2 > 5 && b2 > 5)
+		{
+			var s2 = new FlxSprite();
+			s2.makeGraphic(1, 1, FlxColor.fromRGB(r2, g2, b2, a2));
+			s2.x = px;
+			s2.y = py;
+			add(s2);
+			renders++;
+		}
+
+		px++;
+
+		var p3 = pixels.data.getInt32(4 * (px + py * pixels.width));
+		var a3:Int = p >>> 24;
+		var r3:Int = (p >>> 16) & 0xff;
+		var g3:Int = (p >>> 8) & 0xff;
+		var b3:Int = (p) & 0xff;
+		var hex3:String = StringTools.hex(p3, 8);
+
+		if (r3 > 5 && g3 > 5 && b3 > 5)
+		{
+			var s3 = new FlxSprite();
+			s3.makeGraphic(1, 1, FlxColor.fromRGB(r3, g3, b3, a3));
+			s3.x = px;
+			s3.y = py;
+			add(s3);
+			renders++;
+		}
+
+		px++;
+
+		var p4 = pixels.data.getInt32(4 * (px + py * pixels.width));
+		var a4:Int = p >>> 24;
+		var r4:Int = (p >>> 16) & 0xff;
+		var g4:Int = (p >>> 8) & 0xff;
+		var b4:Int = (p) & 0xff;
+		var hex4:String = StringTools.hex(p4, 8);
+
+		if (r4 > 5 && g4 > 5 && b4 > 5)
+		{
+			var s4 = new FlxSprite();
+			s4.makeGraphic(1, 1, FlxColor.fromRGB(r4, g4, b4, a4));
+			s4.x = px;
+			s4.y = py;
+			add(s4);
+			renders++;
+		}
 
 		px++;
 
